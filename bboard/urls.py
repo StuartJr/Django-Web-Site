e44f9c761870d2afb0ded5d9fa56a3e611d4be73
Stuart_Git <Stuart688@yandex.ru>
2020-03-07 17:44:43 +0400

    Added helper webpage

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import index
+from .views import other_page
 
 app_name='bboard'
 urlpatterns = [
+	path('<str:page>/', other_page, name = 'other'),
 	path('', index, name='index'),
 ]
